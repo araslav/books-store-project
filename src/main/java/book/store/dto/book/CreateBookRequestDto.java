9e79e698c7e1fb38986a94b8ec2d82b7d1a38f1e
Yaroslav Boiko <yaroslav.boiko@ingenios.ro>
2024-02-06 15:57:57 +0200

    Added sprongboot starter validation instead hibernate validation. Implemented strings validation for author and isbn fields by max lenght

--- src/main/java/book/store/dto/book/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/book/CreateBookRequestDto.java
@@ -4,12 +4,16 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import org.hibernate.validator.constraints.Length;
 
 public record CreateBookRequestDto(
         @NotBlank(message = "Title can't be Null or Empty")
         String title,
-        @NotBlank(message = "Author can't be Null") String author,
-        @NotBlank(message = "Isbn can't be Null") String isbn,
+        @Length(max = 12, message = "Author can't be more than 12 chars")
+        @NotBlank(message = "Author can't be Null or Empty") String author,
+        @Length(max = 13, message = "Isbn can't be more than 13 digits")
+        @NotBlank(message = "Isbn can't be Null or Empty")
+        String isbn,
         @NotNull(message = "Price can't be Null")
         @Min(value = 0, message = "Price can't be less 0") BigDecimal price,
         String description,
