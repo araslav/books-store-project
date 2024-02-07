a90b583c758aaba3dc6dd8a33cb9eed79a81e679
Yaroslav Boiko <yaroslav.boiko@ingenios.ro>
2024-02-07 11:34:26 +0200

    Added api's anatations

--- src/main/java/book/store/controller/BookController.java
+++ src/main/java/book/store/controller/BookController.java
@@ -4,11 +4,13 @@ import book.store.dto.book.BookDto;
 import book.store.dto.book.BookSearchParametersDto;
 import book.store.dto.book.CreateBookRequestDto;
 import book.store.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -29,9 +31,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Create a new Book",
+            parameters = {
+                    @Parameter(name = "title", required = true, in = ParameterIn.DEFAULT,
+                            description = "Title of the book",
+                            schema = @Schema(type = "string")),
+                    @Parameter(name = "author", required = true,
+                            description = "Full name of the author",
+                            schema = @Schema(type = "string", maxLength = 12)),
+                    @Parameter(name = "isbn", required = true,
+                            description = "isbn number",
+                            schema = @Schema(type = "string", maxLength = 13)),
+                    @Parameter(name = "price", required = true,
+                            description = "Book's price",
+                            schema = @Schema(type = "number", minimum = "0")),
+                    @Parameter(name = "description",
+                            description = "Book's description",
+                            schema = @Schema(type = "string")),
+                    @Parameter(name = "coverImage",
+                            description = "Book's coverImage",
+                            schema = @Schema(type = "string"))})
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    @Operation(summary = "Create a new Book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.createBook(requestDto);
     }
@@ -48,9 +69,28 @@ public class BookController {
         return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Update Book by id",
+            parameters = {
+                    @Parameter(name = "title", required = true, in = ParameterIn.DEFAULT,
+                            description = "Title of the book",
+                            schema = @Schema(type = "string")),
+                    @Parameter(name = "author", required = true,
+                            description = "Full name of the author",
+                            schema = @Schema(type = "string", maxLength = 12)),
+                    @Parameter(name = "isbn", required = true,
+                            description = "isbn number",
+                            schema = @Schema(type = "string", maxLength = 13)),
+                    @Parameter(name = "price", required = true,
+                            description = "Book's price",
+                            schema = @Schema(type = "number", minimum = "0")),
+                    @Parameter(name = "description",
+                            description = "Book's description",
+                            schema = @Schema(type = "string")),
+                    @Parameter(name = "coverImage",
+                            description = "Book's coverImage",
+                            schema = @Schema(type = "string"))})
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PutMapping("/{id}")
-    @Operation(summary = "Update Book by id")
     public BookDto updateById(@PathVariable Long id,
                               @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.updateById(id, requestDto);
@@ -63,7 +103,7 @@ public class BookController {
         bookService.deleteById(id);
     }
 
-    @Operation(summary = "find Books by params")
+    @Operation(summary = "Find Books by params")
     @GetMapping("/search")
     public List<BookDto> searchBooks(BookSearchParametersDto params) {
         return bookService.search(params);
