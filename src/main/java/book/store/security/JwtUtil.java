b8b821552495348569e56af54e62a1167c5e3635
Yaroslav Boiko <yaroslav.boiko@ingenios.ro>
2024-04-01 13:58:00 +0300

    Added JwtAuthenticationfilter.

--- src/main/java/book/store/security/JwtUtil.java
+++ src/main/java/book/store/security/JwtUtil.java
@@ -1,16 +1,19 @@
 package book.store.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwe;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
-    private Key secret;
+    private SecretKey secret;
     @Value("${jwt.experation}")
     private Long expiration;
 
@@ -20,10 +23,31 @@ public class JwtUtil {
 
     public String generateToken(String email) {
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(secret)
                 .compact();
+    }
+
+    public boolean isValidToken(String token) {
+        Jwe<Claims> claimsJwe = Jwts.parser()
+                .decryptWith(secret)
+                .build()
+                .parseEncryptedClaims(token);
+        return !claimsJwe.getPayload().getExpiration().before(new Date());
+    }
+
+    public String getUsername(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser()
+                .decryptWith(secret)
+                .build()
+                .parseEncryptedClaims(token)
+                .getPayload();
+        return claimsResolver.apply(claims);
     }
 }
